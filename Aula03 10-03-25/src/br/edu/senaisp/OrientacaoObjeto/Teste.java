cca5c0858114611e44fdd167446630146d96392d
nathanrodriguees <nathan_rodrigues07@outlook.com>
2025-03-10 20:25:25 -0300

    feat: Modificadores de Acesso

---
+++ Aula03 10-03-25/src/br/edu/senaisp/OrientacaoObjeto/Teste.java
@@ -0,0 +1,12 @@
+package br.edu.senaisp.OrientacaoObjeto;
+
+import br.edu.senaisp.OrientacaoObjeto.model.Motor;
+
+public class Teste {
+
+	public static void main(String[] args) {
+
+		Motor v8 = new Motor();
+
+	}
+}
---
+++ Aula03 10-03-25/src/br/edu/senaisp/OrientacaoObjeto/model/Motor.java
@@ -0,0 +1,8 @@
+package br.edu.senaisp.OrientacaoObjeto.model;
+
+public class Motor {
+	private String nrSerie;
+	protected String fabricante;
+	public int potencia;
+
+}
