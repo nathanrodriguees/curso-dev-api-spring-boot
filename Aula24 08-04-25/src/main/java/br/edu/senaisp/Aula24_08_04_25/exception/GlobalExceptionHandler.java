9e0f53f38710b7708f3b7cd7514d7b88bac5c42e
nathanrodriguees <nathan_rodrigues07@outlook.com>
2025-04-08 21:54:59 -0300

    feat: Criação do arquivo logback-spring.xml para captura de erros

--- Aula24 08-04-25/src/main/java/br/edu/senaisp/Aula24_08_04_25/exception/GlobalExceptionHandler.java
+++ Aula24 08-04-25/src/main/java/br/edu/senaisp/Aula24_08_04_25/exception/GlobalExceptionHandler.java
@@ -3,12 +3,12 @@ package br.edu.senaisp.Aula24_08_04_25.exception;
 import java.time.LocalDateTime;
 import java.util.Locale;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
-import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -21,52 +21,42 @@ import jakarta.servlet.http.HttpServletRequest;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+	Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
 	@Autowired
 	MessageSource messageSource;
-	
+
 	@Autowired
 	LocaleResolver localeResolver;
-	
+
 	@ExceptionHandler(RecursoNotFound.class)
-	public ResponseEntity<?> 
-	recursoNotFound(RecursoNotFound e, HttpServletRequest request) {
+	public ResponseEntity<?> recursoNotFound(RecursoNotFound e, HttpServletRequest request) {
+
+		log.error(e.getMessage());
 		
 		Locale local = localeResolver.resolveLocale(request);
-		
-	String msgLang = messageSource.getMessage("recurso_notfound"
-									, null, local);
-		
-		MensagemErro msg = 
-				new MensagemErro(
-						LocalDateTime.now()
-					   ,msgLang   
-					   ,request.getContextPath()
-					   );
 
-	return new ResponseEntity<>
-		(msg, HttpStatus.NOT_FOUND);
+		String msgLang = messageSource.getMessage("recurso_notfound", null, local);
+
+		MensagemErro msg = new MensagemErro(LocalDateTime.now(), msgLang, request.getContextPath());
+
+		return new ResponseEntity<>(msg, HttpStatus.NOT_FOUND);
 
 	}
-	
-	@ExceptionHandler(NoResourceFoundException.class)
-	public ResponseEntity<?> 
-			geralNoResourceFoundException(NoResourceFoundException e, 
-						WebRequest request) {
-		System.err.println("AQUI 3");
-	return new ResponseEntity<>
-		(e.getMessage(), HttpStatus.NOT_FOUND);
 
-	}	
-	
+	@ExceptionHandler(NoResourceFoundException.class)
+	public ResponseEntity<?> geralNoResourceFoundException(NoResourceFoundException e, WebRequest request) {
+		System.err.println("AQUI 3");
+		return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+
+	}
+
 	@ResponseStatus(code = HttpStatus.INTERNAL_SERVER_ERROR)
 	@ExceptionHandler(RuntimeException.class)
-	public ResponseEntity<?> 
-			geralRuntimeException(RuntimeException e, 
-						WebRequest request) {
+	public ResponseEntity<?> geralRuntimeException(RuntimeException e, WebRequest request) {
 		System.err.println("AQUI 2");
-	return new ResponseEntity<>
-		(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+		return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
 
-	}	
+	}
 
 }
